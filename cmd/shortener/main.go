50b6e9acb0ec9801c6c60cbb702a725772c78313
Гайфутдинов Эдуард Александрович <gayfutdinov_ea@MacBook-Pro-Apple-2.local>
2022-10-25 14:51:37 +0300

    fix

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env.example file")
 	}
 
 	r := routes.SetupRouter()
