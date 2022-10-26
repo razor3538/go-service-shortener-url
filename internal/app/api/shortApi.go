44ba3193fc6ea17641cee67991ffaaf21106767e
Гайфутдинов Эдуард Александрович <gayfutdinov_ea@MacBook-Pro-Apple-2.local>
2022-10-26 13:18:12 +0300

    добавил поддержку сжатия

--- internal/app/api/shortApi.go
+++ internal/app/api/shortApi.go
@@ -24,7 +24,7 @@ var urlService = services.NewURLService()
 func (sua *ShortURLAPI) ShortenURL(c *gin.Context) {
 	var urlString string
 
-	if strings.Contains(c.Request.Header.Get("Accept-Encoding"), "gzip") {
+	if strings.Contains(c.Request.Header.Get("Accept-Encoding"), "application/gzip") {
 		gz, err := gzip.NewReader(c.Request.Body)
 		if err != nil {
 			tools.CreateError(http.StatusBadRequest, err, c)
