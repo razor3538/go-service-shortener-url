642812a737e1d48f2696d895c3d482c818484a77
Гайфутдинов Эдуард Александрович <gayfutdinov_ea@MacBook-Pro-Apple-2.local>
2022-10-26 12:32:57 +0300

    добавил поддержку сжатия

--- internal/app/api/shortApi.go
+++ internal/app/api/shortApi.go
@@ -9,6 +9,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"io"
 	"net/http"
+	"os"
+	"strings"
 )
 
 type ShortURLAPI struct{}
@@ -22,23 +24,24 @@ var urlService = services.NewURLService()
 func (sua *ShortURLAPI) ShortenURL(c *gin.Context) {
 	var urlString string
 
-	//if strings.Contains(c.Request.Header.Get("Accept-Encoding"), "application/gzip") {
-	//	gz, err := gzip.NewReader(c.Request.Body)
-	//	if err != nil {
-	//		tools.CreateError(http.StatusBadRequest, errors.New("ошибка номер 1"), c)
-	//		return
-	//	}
-	//	defer gz.Close()
-	//
-	//	b, err := io.ReadAll(gz)
-	//	if err != nil {
-	//		tools.CreateError(http.StatusBadRequest, errors.New("ошибка номер 2"), c)
-	//		return
-	//	}
-	//	urlString = string(b)
-	//
-	//} else {
-	//}
+	if strings.Contains(c.Request.Header.Get("Accept-Encoding"), "gzip") {
+		os.Exit(1)
+		//	gz, err := gzip.NewReader(c.Request.Body)
+		//	if err != nil {
+		//		tools.CreateError(http.StatusBadRequest, errors.New("ошибка номер 1"), c)
+		//		return
+		//	}
+		//	defer gz.Close()
+		//
+		//	b, err := io.ReadAll(gz)
+		//	if err != nil {
+		//		tools.CreateError(http.StatusBadRequest, errors.New("ошибка номер 2"), c)
+		//		return
+		//	}
+		//	urlString = string(b)
+		//
+		//} else {
+	}
 
 	b, err := io.ReadAll(c.Request.Body)
 	if err != nil {
