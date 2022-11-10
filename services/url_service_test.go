e2cf7f8899872cba8da3e4f59417d0846a51b5eb
Гайфутдинов Эдуард Александрович <gayfutdinov_ea@MacBook-Pro-Apple-2.local>
2022-11-10 06:46:46 +0300

    fix

--- services/url_service_test.go
+++ services/url_service_test.go
@@ -38,7 +38,7 @@ func TestURLService_Save(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			us := &URLService{}
-			_, err := us.Save(tt.args.urlModel)
+			_, err := us.Save(tt.args.urlModel, "")
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Save() error = %v, wantErr %v", err, tt.wantErr)
 				return
