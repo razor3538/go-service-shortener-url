5e3c1ed9dd16bcd2d9be860aa2b5143c6429e9e6
gayfutdinov_ea <gayfutdinov_ea@itms.tech>
2023-04-27 16:51:00 +0300

    fix

--- internal/tools/loger.go
+++ internal/tools/loger.go
@@ -5,6 +5,8 @@ import (
 	"os"
 )
 
+// InfoLog global var that give a info logger methods
 var InfoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 
+// ErrorLog global var that give a error logger methods
 var ErrorLog = log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
